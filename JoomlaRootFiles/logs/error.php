e836d01e133e67add1aaf7e6817049c580ef8a24
Abdulhadi Waheed <abdul-hadi99@windowslive.com>
2015-10-13 23:12:42 +0300

    2 change made
    (1) Added Joomla Root Files
    (2) Added Joomla Database

---
+++ JoomlaRootFiles/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-10-12 17:38:52 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-10-12T17:38:52+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-10-13T18:52:26+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
