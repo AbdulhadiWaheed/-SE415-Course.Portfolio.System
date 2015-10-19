dea19d059df001ec43d39bb80cd427d975afc908
omarObeid <o2-1993@hotmail.com>
2015-10-19 06:41:42 +0300

    Latest Database
    This is the database for the last commit

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-10-19 03:35:33 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-10-19T03:35:33+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-10-19T03:35:51+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
